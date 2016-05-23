493f8987c3985a10a0be5c2f1ab392387485d004
Daniel Azuma <dazuma@gmail.com>
2016-05-22 22:30:31 -0700

    dazuma chapter 4

--- dazuma/rumbl/lib/rumbl/repo.ex
+++ dazuma/rumbl/lib/rumbl/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "José Valim", username: "josevalim", password: "elixir"},
-      %Rumbl.User{id: "2", name: "Bruce Tate", username: "redrapids", password: "7langs"},
-      %Rumbl.User{id: "3", name: "Chris McCord", username: "chrismccord", password: "phx"},
-      %Rumbl.User{id: "3", name: "Daniel Azuma", username: "dazuma", password: "meow"},
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    module
-      |> all
-      |> Enum.find(&(&1.id == id))
-  end
-
-  def get_by(module, params) do
-    module
-      |> all
-      |> Enum.find(fn map ->
-        Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-      end)
-  end
-
+  use Ecto.Repo, otp_app: :rumbl
 end
