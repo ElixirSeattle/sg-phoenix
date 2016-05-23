8eb0c95ee678951a937b21f07ea138e75acd8b3e
Marc Schwieterman <marc@marcisme.com>
2016-05-22 18:32:12 -0700

    Add chapter 4 work

--- marcisme/rumbl/web/controllers/user_controller.ex
+++ marcisme/rumbl/web/controllers/user_controller.ex
@@ -1,5 +1,23 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
