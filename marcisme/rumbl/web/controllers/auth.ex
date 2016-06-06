f0f31d205a152898edcdc1916875ce62ac7a170e
Marc Schwieterman <marc@marcisme.com>
2016-06-06 19:41:03 -0400

    Add chapter 6 work

--- marcisme/rumbl/web/controllers/auth.ex
+++ marcisme/rumbl/web/controllers/auth.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -36,5 +38,16 @@ defmodule Rumbl.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
--- marcisme/rumbl/web/controllers/user_controller.ex
+++ marcisme/rumbl/web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
   alias Rumbl.User
 
   def new(conn, _params) do
@@ -29,16 +29,5 @@ defmodule Rumbl.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
   end
 end
