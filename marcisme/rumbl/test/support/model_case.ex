bc49dfdb3aa87f4b803afb214dc26d5a0a19d232
Marc Schwieterman <marc@marcisme.com>
2016-06-20 18:37:06 -0700

    Add chapter 8 work

--- marcisme/rumbl/test/support/model_case.ex
+++ marcisme/rumbl/test/support/model_case.ex
@@ -21,6 +21,7 @@ defmodule Rumbl.ModelCase do
       import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
+      import Rumbl.TestHelpers
       import Rumbl.ModelCase
     end
   end
