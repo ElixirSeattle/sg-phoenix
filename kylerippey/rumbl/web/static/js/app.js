43c8d74e523df88d4a0323b4b3f3f861212877a0
Kyle Rippey <kylerippey@gmail.com>
2016-06-21 21:22:12 -0700

    Chapter 9

--- kylerippey/rumbl/web/static/js/app.js
+++ kylerippey/rumbl/web/static/js/app.js
@@ -19,3 +19,12 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Player from "./player"
+let video = document.getElementById("video")
+
+if(video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready!")
+  })
+}
