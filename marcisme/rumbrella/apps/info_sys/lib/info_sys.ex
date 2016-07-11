aeb55747266b3045c9d93f1362d194b7e8783c97
Marc Schwieterman <marc@marcisme.com>
2016-07-10 19:28:03 -0700

    Add chapter 12 work

--- marcisme/rumbrella/apps/info_sys/lib/info_sys.ex
+++ marcisme/rumbrella/apps/info_sys/lib/info_sys.ex
@@ -1,5 +1,11 @@
-defmodule Rumbl.InfoSys do
-  @backends [Rumbl.InfoSys.Wolfram]
+defmodule InfoSys do
+  use Application
+
+  def start(_type, _args) do
+    InfoSys.Supervisor.start_link()
+  end
+
+  @backends [InfoSys.Wolfram]
 
   defmodule Result do
     defstruct score: 0, text: nil, url: nil, backend: nil
@@ -22,7 +28,7 @@ defmodule Rumbl.InfoSys do
   defp spawn_query(backend, query, limit) do
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
-    {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
+    {:ok, pid} = Supervisor.start_child(InfoSys.Supervisor, opts)
     monitor_ref = Process.monitor(pid)
     {pid, monitor_ref, query_ref}
   end
